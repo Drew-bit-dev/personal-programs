465e8c2b9564cef8af47ba6e020876e9a0e915ec
websitehosting123 <d.s.grondahl01@gmail.com>
2022-02-02 15:12:07 -0600

    Update password_inputer_code(fromThonny).py

--- password_inputer_code(fromThonny).py
+++ password_inputer_code(fromThonny).py
@@ -10,11 +10,11 @@ time.sleep(2)
 
 layout.write(' ')
 time.sleep(3)
-layout.write('INPUT PASSWORD HERE')
+layout.write('INPUT PASSWORD / TEXT HERE')
 time.sleep(1)
 layout.write('\n')
 
 #to run this you will have to 
-#import the keyboard inputs 
+#copy this code into Thonny
 #as well as download the 
 #microcontroller library from Adafruit
